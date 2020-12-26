e0ccd109c6da937896b368a19e6a9e860fbbece0
johnliu123 <john29030134@gmail.com>
2020-12-26 08:21:52 +0800

    app.py debug 手機修改價格

--- app.py
+++ app.py
@@ -11,17 +11,6 @@ from linebot.models import *
 import mongodb
 import re
 
-#from __future__ import print_function
-import time
-from linebot import (LineBotApi, WebhookHandler, exceptions)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import *
-import schedule
-from pymongo import MongoClient
-import urllib.parse
-import datetime
-import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
