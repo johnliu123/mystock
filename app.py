4755f68737508b3de51eeee3bd6750c595031d6c
johnliu123 <john29030134@gmail.com>
2020-12-27 10:36:24 +0800

    app.py debug 定時抓取股價測試

--- app.py
+++ app.py
@@ -65,6 +65,12 @@ def handle_message(event):
     
 #即時抓價格    
 def handle_message_price(event):
+    
+    #取得顧客資訊
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id #使用者ID
+    usespeak=str(event.message.text) #使用者講的話
+    
     if re.match('[0-9]{4}價格',usespeak): # 先判斷是否是使用者要用來存股票的
         data = mongodb.show_user_stock_fountion()
 
