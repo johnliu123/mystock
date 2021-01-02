4165925414a2966c1a29e5cc0d5d554bd70007ca
johnliu123 <john29030134@gmail.com>
2021-01-03 02:54:49 +0800

    app.py 使用Carousel_template (綜合多個ButtonsTemplate) 產業類股 測試

--- app.py
+++ app.py
@@ -467,18 +467,6 @@ def handle_postback(event):
                             ),
                     
                 ]
-            ),
-            CarouselColumn(
-                title='請輸入類股代號：',
-                text='請選擇產業類股',
-                actions=[
-                    PostbackAction(
-                            label='34.農業科技業',
-                            text='34.農業科技業',
-                            data='農業科技業'
-                            ),
-                    
-                ]
             )
         ]
     )
