6a638ebb9d43eaec79712735eed3d902a18120b6
johnliu123 <john29030134@gmail.com>
2021-01-09 00:20:37 +0800

    app.py 爬蟲股票 本益比測試 debug(使用者代理)

--- app.py
+++ app.py
@@ -62,15 +62,17 @@ def test ():
 
 def stock ():
     
-    import time
+    #import time
     #import schedule
     #from pymongo import MongoClient
     #import urllib.parse
     #import datetime
     import re
     import numpy as np
-    
+    from fake_useragent import UserAgent
     import random
+    
+    user_agent = UserAgent()
     
     # 要抓取的網址
     url = 'https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=全部&INDUSTRY_CAT=半導體業&SHEET=交易狀況&SHEET2=日&RPT_TIME=最新資料'
@@ -88,8 +90,8 @@ def stock ():
         "Sec-Fetch-Site": "none", 
         "Upgrade-Insecure-Requests": "1", 
         #隨機設定 使用者代理(User-Agent)
-        #"User-Agent":user_agent.random,
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36", #使用者代理
+        "User-Agent":user_agent.random,
+        #"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36" #使用者代理
         "Referer": "https://www.google.com/"
     }
     
