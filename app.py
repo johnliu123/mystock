342f66227ad4bd348684e8c8c39a3c6c9e2273b8
johnliu123 <john29030134@gmail.com>
2021-01-10 15:35:40 +0800

    app.py 爬蟲股票 本益比測試 (部份程式) debug 測試 是否有抓到資料

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def stock ():
         #隨機設定 使用者代理(User-Agent)
         #"User-Agent":user_agent.random,
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36", #使用者代理
+        "Referer": "https://www.google.com/"
         
     }
     
