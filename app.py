102d641eccef35ba06924d648c75310d0ef168eb
johnliu123 <john29030134@gmail.com>
2021-01-10 09:24:23 +0800

    app.py 爬蟲股票 本益比測試 (部份程式) debug request post UserAgent  random 測試

--- app.py
+++ app.py
@@ -69,21 +69,21 @@ def stock ():
     #import datetime
     #import re
     #import numpy as np
-    #from fake_useragent import UserAgent
+    from fake_useragent import UserAgent
     #import random
     
     # 要抓取的網址
     url = 'https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=全部&INDUSTRY_CAT=水泥工業&SHEET=交易狀況&SHEET2=日&RPT_TIME=最新資料'
     
     
-    #user_agent = UserAgent()
+    user_agent = UserAgent()
     
     headers = {
         
        
         #隨機設定 使用者代理(User-Agent)
-        #"User-Agent":user_agent.random,
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36", #使用者代理
+        "User-Agent":user_agent.random,
+        #"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36", #使用者代理
         
     }
     
