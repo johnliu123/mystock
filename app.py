a37be23a0921e7d843a6e46ceb380dcfe0acf3b0
johnliu123 <john29030134@gmail.com>
2021-01-10 15:10:41 +0800

    app.py 爬蟲股票 本益比測試 (部份程式) debug 測試 股票代碼

--- app.py
+++ app.py
@@ -79,8 +79,16 @@ def stock ():
     #user_agent = UserAgent()
     
     headers = {
-        
-       
+            
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+        "Accept-Encoding": "gzip, deflate, br", 
+        "Accept-Language": "zh-TW,zh;q=0.9", 
+        #"Host": "goodinfo.tw/StockInfo/index.asp",  #目標網站 
+        "Sec-Fetch-Dest": "document", 
+        "Sec-Fetch-Mode": "navigate", 
+        "Sec-Fetch-Site": "none", 
+        "Upgrade-Insecure-Requests": "1", 
         #隨機設定 使用者代理(User-Agent)
         #"User-Agent":user_agent.random,
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36", #使用者代理
