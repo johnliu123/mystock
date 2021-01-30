1aaf160b306e09281a883f36981c6dc66592a39c
johnliu123 <john29030134@gmail.com>
2021-01-30 08:32:02 +0800

    app.py 爬蟲股票 本益比測試 (部份程式 list_req1) debug 測試 是否有抓到資料

--- app.py
+++ app.py
@@ -150,11 +150,12 @@ def stock_crab():
         try:
             url1='http://jsjustweb.jihsun.com.tw/z/zc/zca/zca_'+num+'.djhtm'
             
-            result=url1
-                
-            return result
+            
             #請求網站
             list_req1 = requests.post(url1, headers = headers)
+            result=list_req1
+                
+            return result
             #將整個網站的程式碼爬下來
             soup1 = BeautifulSoup(list_req1.text, "html.parser")
             tr=soup1.find_all('tr')[6]
