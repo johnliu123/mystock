cf29cf425d7fbd46382e1d97f105acaf3608e8fc
johnliu123 <john29030134@gmail.com>
2021-01-30 09:48:37 +0800

    app.py 爬蟲股票 本益比測試 (部份程式 list_req1) debug 測試 是否有抓到資料

--- app.py
+++ app.py
@@ -152,12 +152,14 @@ def stock_crab():
             
             
             #請求網站
+            #list_req1 = requests.post(url1, headers = headers)
+            #請求網站
             list_req1 = requests.post(url1, headers = headers)
             #將整個網站的程式碼爬下來
             soup1 = BeautifulSoup(list_req1.text, "html.parser")
-            result=soup1
+            #result=soup1
                 
-            return result
+            return list_req1
             tr=soup1.find_all('tr')[6]
             td=tr.find_all('td')[1]
             PBR= td.text
