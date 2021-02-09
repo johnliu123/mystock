464ba87fdc11cc3e35b0ff5942719bc485fe9389
johnliu123 <john29030134@gmail.com>
2021-02-10 07:33:43 +0800

    app.py 新增股票類型名稱(用 line_bot_api.reply_message 儲存 股票類型) industy

--- app.py
+++ app.py
@@ -858,8 +858,8 @@ def handle_postback(event):
         #industry=event.postback.data
         industry="水泥工業"
         
-        industry_list=[]
-        industry_list.append(industry)
+        #industry_list=[]
+        #industry_list.append(industry)
         
         
         stock_project_template=stock_propose_template()
@@ -914,11 +914,12 @@ def handle_postback(event):
         #if 判斷 是否 industry＝水泥工業 傳到stock_crab方法裡
         result=stock_crab()
         
-        for i in industry_list:
-            name=i
+        #for i in industry_list:
+            #name=i
+        
         
         #params = {"message":"水泥工業"+result}
-        params = {"message":stock_project_template}
+        params = {"message":line_bot_api.reply_message(event.reply_token,stock_project_template)}
         r = requests.post("https://notify-api.line.me/api/notify",
                                           headers=headers2, params=params)
         
