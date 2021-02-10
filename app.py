5940b7c068d3225520bc2f2dacf3ca9d56f2b9c4
johnliu123 <john29030134@gmail.com>
2021-02-10 13:27:39 +0800

    app.py 測試 uid

--- app.py
+++ app.py
@@ -378,7 +378,7 @@ def handle_message(event):
         return 0
     
     elif re.match('測試',usespeak): # 取得id
-        line_bot_api.push_message(uid,TextSendMessage("你的id"+event.source.userId))
+        line_bot_api.push_message(uid,TextSendMessage("你的id"+uid))
         
         return 0
     
