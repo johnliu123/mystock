3ece2477512feed14d5faab8d08f9033f367f2b4
johnliu123 <john29030134@gmail.com>
2021-02-13 08:18:47 +0800

    app.py修改 user TextSendMessage

--- app.py
+++ app.py
@@ -383,8 +383,10 @@ def handle_message(event):
         
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text="請輸入您的姓名"))
         #line_bot_api.push_message(uid,TextSendMessage("請輸入您的姓名"))
+        
         username=str(event.message.text)
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="已輸入"+username))
+        if username=="test":
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="已輸入"+username))
         """
         if re.match('[^0-9]',username):
         #if event.message.text !="":
