4aeec36312823405d154129b54d74f3ba972ca36
johnliu123 <john29030134@gmail.com>
2021-03-12 23:46:21 +0800

    合併 method Headers 測試

--- stockmd.py
+++ stockmd.py
@@ -8,7 +8,7 @@ Created on Thu Mar 11 23:19:42 2021
 
 import requests
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
+#from fake_useragent import UserAgent
 
 def get_headers():
     
@@ -16,21 +16,21 @@ def get_headers():
     
     #user_agent = UserAgent()
     headers = {
-        "Authorization": "Bearer " + "YkrXjA4k7pswPML2wkdNxgcRhqSKPcrBysvLmIClsvd",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-        "Accept-Encoding": "gzip, deflate, br", 
-        "Accept-Language": "zh-TW,zh;q=0.9", 
-        #"Host": "goodinfo.tw/StockInfo/index.asp",  #目標網站 
-        "Sec-Fetch-Dest": "document", 
-        "Sec-Fetch-Mode": "navigate", 
-        "Sec-Fetch-Site": "none", 
-        "Upgrade-Insecure-Requests": "1", 
-        #隨機設定 使用者代理(User-Agent)
-        #"User-Agent":user_agent.random,
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36", #使用者代理
-        "Referer": "https://www.google.com/"
-        
+    
+    "Authorization": "Bearer " + "YkrXjA4k7pswPML2wkdNxgcRhqSKPcrBysvLmIClsvd",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+    "Accept-Encoding": "gzip, deflate, br", 
+    "Accept-Language": "zh-TW,zh;q=0.9", 
+    #"Host": "goodinfo.tw/StockInfo/index.asp",  #目標網站 
+    "Sec-Fetch-Dest": "document", 
+    "Sec-Fetch-Mode": "navigate", 
+    "Sec-Fetch-Site": "none", 
+    "Upgrade-Insecure-Requests": "1", 
+    #隨機設定 使用者代理(User-Agent)
+    #"User-Agent":user_agent.random,
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+    "Referer": "https://www.google.com/"
     }
 
     return headers
